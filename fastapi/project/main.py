95acf3a033c3e8345a3078009bc0b79199dfef5c
Zaid Rasool <zaidrasool56@gmail.com>
2025-08-10 22:03:36 +0500

    fastapi setup

--- fastapi/project/main.py
+++ fastapi/project/main.py
@@ -1,1 +1,9 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import joblib
+
+model = joblib.load("model.joblib")
+app = FastAPI(title="California Housing Price Prediction API")
+
+
 
