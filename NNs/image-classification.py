107e626d5df2955a133e96cf9f6f3853449229f9
Zaid Rasool <zaidrasool56@gmail.com>
2025-10-08 18:50:43 +0500

    model architecture

--- NNs/image-classification.py
+++ NNs/image-classification.py
@@ -24,3 +24,14 @@ training_labels = training_labels[:20000]
 test_images = test_images[:4000]
 test_labels = test_labels[:4000]
 
+# Model architecture
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.Flatten())
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(10, activation='softmax'))
+
