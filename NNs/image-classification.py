4458efd4ebcbe5a79ae8b882f00632beff74fe29
Zaid Rasool <zaidrasool56@gmail.com>
2025-10-08 18:18:20 +0500

    show images with labes

--- NNs/image-classification.py
+++ NNs/image-classification.py
@@ -3,5 +3,17 @@ import numpy as np
 from tensorflow.keras import datasets, layers, models
 import matplotlib.pyplot as plt
 
+# Load and preprocess the CIFAR-10 dataset
 (training_images, training_labels), (test_images, test_labels) = datasets.cifar10.load_data()
 training_images, test_images = training_images / 255, test_images / 255
+
+# classes or features
+class_names = ['airplane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+for i in range(16):
+    plt.subplot(4,4,i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.inshow(training_images[i], cmap=plt.cm.binary)
+    plt.xlabel(class_names[training_labels[i][0]])
+
+plt.show()
