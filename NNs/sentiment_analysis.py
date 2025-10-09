98fa934cfb37f514850e8f97d1e45335f3fa3f15
Zaid Rasool <zaidrasool56@gmail.com>
2025-10-09 16:01:52 +0500

    fix the fix

--- NNs/sentiment_analysis.py
+++ NNs/sentiment_analysis.py
@@ -4,11 +4,9 @@ import nltk
 
 url = "https://en.wikipedia.org/wiki/Mathematics"
 article = Article(url)
-nltk.download('punkt')
-nltk.download('punkt_tab')
 article.download() # Download the article 
 article.parse() # readable
 article.nlp() # prep for analysis
-
+# text = article.summary
 text = article.text # get the text 
 print(text)
