87b9e60822d44f4527b37ca538cb9e26679403bb
Zaid Rasool <zaidrasool56@gmail.com>
2025-10-09 15:55:22 +0500

    download article and get text

--- NNs/sentiment_analysis.py
+++ NNs/sentiment_analysis.py
@@ -1,2 +1,11 @@
 from textblob import TextBlob
-from newspaper import Article 
+from newspaper import Article
+
+url = "https://en.wikipedia.org/wiki/Mathematics"
+article = Article(url)
+article.download() # Download the article 
+article.parse() # readable
+article.nlp() # prep for analysis
+
+text = article.text # get the text 
+print(text)
