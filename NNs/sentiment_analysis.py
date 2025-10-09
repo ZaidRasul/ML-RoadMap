ae2c5b84ffd431c7e71de427d808631a5a56e873
Zaid Rasool <zaidrasool56@gmail.com>
2025-10-09 16:29:52 +0500

    sentiment analysis of shehbaz shareef speeches

--- NNs/sentiment_analysis.py
+++ NNs/sentiment_analysis.py
@@ -1,15 +1,20 @@
 from textblob import TextBlob
-from newspaper import Article
+
+
+''' from newspaper import Article
 import nltk
 
-url = "https://en.wikipedia.org/wiki/Mathematics"
+url = "https://www.cnbc.com/2020/04/22/recession-depth-will-be-much-worse-than-2007-2009-lakshman-achuthan.html"
 article = Article(url)
 article.download() # Download the article 
 article.parse() # readable
 article.nlp() # prep for analysis
 # text = article.summary
 text = article.text # get the text 
-print(text)
+print(text)''' 
+
+with open('NNs/sentiment_analysis_text.txt', 'r') as file:
+    text = file.read()
 
 blob = TextBlob(text)
 sentiment = blob.sentiment.polarity
