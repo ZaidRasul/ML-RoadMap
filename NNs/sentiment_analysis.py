205011182f58e642a0ace1222edeeadc4a064df5
Zaid Rasool <zaidrasool56@gmail.com>
2025-10-09 15:59:01 +0500

    fix

--- NNs/sentiment_analysis.py
+++ NNs/sentiment_analysis.py
@@ -1,8 +1,11 @@
 from textblob import TextBlob
 from newspaper import Article
+import nltk
 
 url = "https://en.wikipedia.org/wiki/Mathematics"
 article = Article(url)
+nltk.download('punkt')
+nltk.download('punkt_tab')
 article.download() # Download the article 
 article.parse() # readable
 article.nlp() # prep for analysis
