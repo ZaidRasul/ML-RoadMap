5f9c8baf3dab4b283cc7a24706a5ee3d94a23880
Zaid Rasool <zaidrasool56@gmail.com>
2025-10-11 15:13:02 +0500

    destructor for camera

--- NNs/cam-image-classifier/camera.py
+++ NNs/cam-image-classifier/camera.py
@@ -1,1 +1,14 @@
 import cv2 as cv
+
+class Camera:
+    def __init__(self):
+        self.camera = cv.VideoCapture(0)
+        if not self.camera.isOpened():
+            raise Exception("Could not open video device")
+        
+    def __del__(self):# destructor to close the camera
+        if self.camera.isOpened():
+            self.camera.release()
+
+    def get_frame(self):
+        pass
