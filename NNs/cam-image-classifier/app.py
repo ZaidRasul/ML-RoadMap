323ce3053eb2dda6de431d2c2b6887336f66bbf6
Zaid Rasool <zaidrasool56@gmail.com>
2025-10-11 21:19:34 +0500

    construcor for app class

--- NNs/cam-image-classifier/app.py
+++ NNs/cam-image-classifier/app.py
@@ -5,3 +5,21 @@ import os
 import cv2 as cv
 import camera
 
+class App:
+    def __init__(self, window=tk.Tk(), window_title="Cam-Image_Classifier"):
+        self.window = window
+        self.window_title = window_title
+
+        self.counters = [1, 1]
+        #self.model = something
+        self.auto_predict = False
+
+        self.camera = camera.Camera()
+        #self.init_gui()
+        self.delay = 15
+        #self.update()
+        self.window.attributes('-topmost', True)
+        self.window.mainloop()
+
+    
+        
