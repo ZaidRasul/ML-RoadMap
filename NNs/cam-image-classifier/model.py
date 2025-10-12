1eaa3f1c55846480de30f0eaec35a4fc9377c654
Zaid Rasool <zaidrasool56@gmail.com>
2025-10-12 21:37:20 +0500

    fix the size issue

--- NNs/cam-image-classifier/model.py
+++ NNs/cam-image-classifier/model.py
@@ -6,19 +6,21 @@ import PIL
 class Model:
     def __init__(self):
         self.model = LinearSVC()
-        self.trained = False
+        #self.trained = False
 
     def train_model(self, counters):
         img_list = np.array([])
         class_list = np.array([])
         for i in range (1, counters[0]):
             img = cv.imread(f"1/frame{i}.jpg")[:,:,0]
+            img = cv.resize(img, (150, 112))
             img = img.reshape(16800) # flattening the image
             img_list = np.append(img_list, img)
             class_list = np.append(class_list, 1)
 
         for i in range (1, counters[1]):
             img = cv.imread(f"2/frame{i}.jpg")[:,:,0]
+            img = cv.resize(img, (150, 112))
             img = img.reshape(16800) # flattening the image
             img_list = np.append(img_list, img)
             class_list = np.append(class_list, 2)
