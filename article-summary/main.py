59fcbe7a3f376f3d70bb9b4d51809da1d9392ca2
Zaid Rasool <zaidrasool56@gmail.com>
2025-10-15 22:12:48 +0500

    dowload and prep data

--- article-summary/main.py
+++ article-summary/main.py
@@ -3,3 +3,8 @@ from textblob import TextBlob
 import nltk
 from newspaper import Article
 
+url = "https://edition.cnn.com/2025/10/13/tech/openai-broadcom-power"
+article = Article(url)
+article.download()
+article.parse()
+article.nlp()
