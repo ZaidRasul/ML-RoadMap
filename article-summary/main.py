1246117699d797953f72edb516aa8a0b077da60a
Zaid Rasool <zaidrasool56@gmail.com>
2025-10-15 22:30:01 +0500

    comment

--- article-summary/main.py
+++ article-summary/main.py
@@ -16,5 +16,8 @@ print("Authors:", article.authors)
 print("Publication Date:", article.publish_date)
 print("Summary:", article.summary)
 
+# sentiment analysis gives polarity and subjectivity
+# polarity: -1 (negative) to 1 (positive)
+# subjectivity: 0 (objective) to 1 (subjective) [measure of personal opinion]
 sentiment = tb(article.text).sentiment
 print("Sentiment:", sentiment)
